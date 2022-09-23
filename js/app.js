aa08daa5c673e281ba994cdaab3ca5605082fd49
hidao80 <hidao80@users.noreply.github.com>
2022-09-23 15:05:12 +0900

    fix: Failed to initialize.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 (async () => {
     // Load and show target domain list
-    const domainList = await chrome.storage.sync.get('domainList').then(item => item['domainList']);
+    const domainList = await chrome.storage.sync.get('domainList').then(item => item['domainList']) ?? "https://misskey.dev";
     const domainArray = domainList.split("\n");
 
     // Do not run if you are on a page that is not in the target domain list.
-    console.log(document.domain);
+    // console.log(document.domain);
     if (domainArray.filter(v => (new RegExp(document.domain)).test(v)).length) {
         const functionNames = ['TimeToAbsolute', 'NotesSpeech', 'FediverseTicker', 'KeywordsFilter', 'NotificationBeep'];
         for (const name of functionNames) {
